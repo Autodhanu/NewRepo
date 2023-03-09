ee4e8e05aca984a2a06716e62621ce033266c7bf
Dhananjaya BN <dhananjaya.bn@mphasis.com>
2023-03-09 21:31:38 +0530

    added/updated/deleted files

--- src/Pages/LoginPage.java
+++ src/Pages/LoginPage.java
@@ -4,6 +4,10 @@ public class LoginPage
 	public static void main(String a[])
 	{
 		System.out.println("hello - LP");
+		
+		String un ="admin";
+		String pwd ="admin";
+		System.out.println("the logged inn --");
 	}
 
 }
---
+++ src/Pages/SearchPage.java
@@ -0,0 +1,10 @@
+package Pages;
+
+public class SearchPage {
+
+	public void search() {
+		// TODO Auto-generated method stub
+          System.out.println("search");
+	}
+
+}
