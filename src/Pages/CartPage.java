ca6567a1fd37debdcf7693f4d871aab4a6fa8bb8
Dhananjaya BN <dhananjaya.bn@mphasis.com>
2023-03-13 19:29:02 +0530

    updated cart page changes

--- src/Pages/CartPage.java
+++ src/Pages/CartPage.java
@@ -5,6 +5,7 @@ public class CartPage {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
         System.out.println("cart page");
+        System.out.println("cart page feature");
 	}
 
 }
