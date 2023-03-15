99a392ae708a617f89cfd3aa0244e02db265c775
Dhananjaya BN <dhananjaya.bn@mphasis.com>
2023-03-15 14:42:41 +0530

    reg page action code

---
+++ src/Pages/RegistrationPageActions.java
@@ -0,0 +1,11 @@
+package Pages;
+
+public class RegistrationPageActions {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("end to end automation");
+	}
+
+}
